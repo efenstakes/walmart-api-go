2eff100206119eeaa00cae9e83f97766814c8e56
Efen Stakes <efenstakes101@gmail.com>
2023-07-20 11:48:53 +0300

    add cart model

---
+++ cart/model.go
@@ -0,0 +1,16 @@
+package cart
+
+import (
+	"time"
+
+	"github.com/kamva/mgm/v3"
+)
+
+type Cart struct {
+	mgm.DefaultModel `bson:",inline"`
+
+	UserId    string    `bson:"userId" json:"userId"`
+	Price     float64   `bson:"price" json:"price"`
+	ProductID string    `bson:"productId" json:"productId"`
+	SavedOn   time.Time `bson:"savedOn" json:"savedOn"`
+}
